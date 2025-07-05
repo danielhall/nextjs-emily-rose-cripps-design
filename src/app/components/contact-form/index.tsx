dc29a503750c3aa041166971f59e6297ddb3e875
Daniel Hall <daniel.jh310@gmail.com>
2025-07-05 19:18:52 +0100

    Remove unused RadixLabel import from ContactForm component

--- src/app/components/contact-form/index.tsx
+++ src/app/components/contact-form/index.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState } from 'react';
-import * as RadixLabel from '@radix-ui/react-label';
 import { motion, AnimatePresence } from 'framer-motion';
 import Image from 'next/image';
 
